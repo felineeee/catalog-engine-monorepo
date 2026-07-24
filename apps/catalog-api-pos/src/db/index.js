7dd0338fffb2eb3c35ac3eba8feff1182cd6e58f
kafi <672021131@student.uksw.edu>
2026-07-24 12:03:57 +0700

    -Minor fix

--- apps/catalog-api-pos/src/db/index.js
+++
@@ -1,5 +0,0 @@
-import { createDatabaseClient } from '@catalog-engine/database';
-import * as dotenv from 'dotenv';
-dotenv.config();
-export const db = createDatabaseClient(process.env.DATABASE_URL);
-export const replicaDb = createDatabaseClient(process.env.DATABASE_URL_REPLICA, { max: 40 });
--- apps/catalog-api-pos/src/search/index.js
+++
@@ -1,4 +0,0 @@
-import * as dotenv from 'dotenv';
-import { createMeilisearchClient } from '@catalog-engine/search';
-dotenv.config();
-export const productIndex = createMeilisearchClient(process.env.MEILISEARCH_HOST || 'http://localhost:7700', process.env.MEILISEARCH_KEY || 'supersecret_meili', 'products');
